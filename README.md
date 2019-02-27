6348a69d6c50b044f60a873accb97cfdd6634013
lalitha <lalithapelluri6@gmail.com>
2019-02-26 21:40:29 -0800

    changes done to readme and js

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ### User clicks on crytal shows the random number in the numberdiv.
 #### each crystal assigned some value by default.
 ##### when ever user clicks on the crystal corresponding value will be added to totalscore.
-###### if the total score is less than the random score then user win.
+###### if the total score is less than the random score then user wins.
 ####### if total socre is greater than the random score then user loose.
 ######## this game is designed using HTML, CSS, JQUERY, JS.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,89 @@
-var randomScore = 40;
-var wins = 0;
-var loss = 0;
-var crystalClicked = false;
-var crystals = ['crystalgreen', 'crystalblue', 'crystalyellow', 'crystalred'];
-var totalScore = 0;
-var crystalvalue = [4, 2, 1, 7];
-[crystals] = [crystalvalue];
 
-$(document).ready(function () {
-    crystalClicked = true;
-    $(".game-crystal").on("click", function () {
-        for (var i = 0; i < crystals.length; i++) {
-            if (crystalClicked === true && crystalClicked === crystals[i]) {
-                $('.number').text(randomScore)
-                crystalscore = crystal[i].val();
-                totalScore = totalScore + crystalscore;
-                console.log(totalScore);
-                wins++
-                $('#win').text("wins" + "" + wins)
-            } else
-                if (crystalClicked === true && crystalClicked === crystals[i] && totalScore > randomScore) {
-                  loss++
-                    $('#loss').text("loss" + " " + loss)
-                }
-        }
+$(document).ready(function(){
+var additionFromCrystals = 0; 
+    var userWins = 0;
+    var userLoses = 0;
+    var randomCrystalOne = 0;
+    var randomCrystalTwo = 0;
+    var randomCrystalThree = 0;
+    var randomCrystalFour = 0;
+    var randomNumberToMatch = 0;
+
+    //Reset values              
+
+    function resetAll() {
+        numberToMatch();
+        randomGenerator();
+        additionFromCrystals = 0;
+
+        $(".score").text(additionFromCrystals);
+        $(".number").text(numberToMatch);
+    }
+
+    //Generate random number to match between 19 - 120
+
+    var numberToMatch = function(){
+        randomNumberToMatch = Math.floor((Math.random() * 120) + 19); 
+        
+        //Place the number on the DOM
+        
+        $(".number").html(randomNumberToMatch);
+    }
+
+    //Generate random numbers for each crystal
+
+    var randomGenerator = function(){
+        randomCrystalOne = Math.floor((Math.random() * 12) + 1);
+        randomCrystalTwo = Math.floor((Math.random() * 12) + 5);
+        randomCrystalThree = Math.floor((Math.random() * 12) + 1);
+        randomCrystalFour = Math.floor((Math.random() * 12) + 4);
+    }
+
+    //Adding to the score
+
+    function addingToScore(crystalNumber){
+        additionFromCrystals = additionFromCrystals + crystalNumber;
+     }
+
+    //Calling functions
+
+    numberToMatch();
+    randomGenerator();
+
+    $(".game-crystal").on("click", function(){
+        addingToScore(randomCrystalOne);
+        $(".score").text(additionFromCrystals);
+       checkScore(); 
+
     });
+
+    $(".game-crystal").on("click", function(){
+        addingToScore(randomCrystalTwo);
+        $(".score").text(additionFromCrystals);
+        checkScore();
+    });
+
+    $(".game-crystal").on("click", function(){
+        addingToScore(randomCrystalThree);
+        $(".score").text(additionFromCrystals);
+        checkScore();
+    });
+
+    $(".game-crystal").on("click", function(){
+        addingToScore(randomCrystalFour);
+        $(".score").text(additionFromCrystals);
+        checkScore();
+    });
+   
+    var checkScore = function(){
+        if (additionFromCrystals === randomNumberToMatch){
+            resetAll();
+            userWins++;
+            $("#win").text('Wins:' + userWins);
+        } else if (additionFromCrystals > randomNumberToMatch){
+            resetAll();
+            userLoses++;
+            $("#loss").text('Loses:' + userLoses);
+        }
+    }    
 });
